7500b4028122157f8ad377a188d8171c5ecf698a
js340 <jaskaransingh3433@gmail.com>
2022-01-01 21:04:07 +0000

    difficulty and speed drop down menu and slider
    not implemented yet.

---
+++ Assets/Scripts/DifficultyDropDownMenu.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DifficultyDropDownMenu : MonoBehaviour
+{
+    public void HandleInputData(int val)
+    {
+        if (val == 0){
+            // difficulty set to easy
+        } 
+        if (val == 1){
+            // difficulty set to medium
+        } 
+        if (val == 2){
+            // difficulty set to hard
+        } 
+    }
+}
