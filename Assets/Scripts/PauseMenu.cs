a0518816fd5fb68522f15589535ff469bfcbf005
js340 <jaskaransingh3433@gmail.com>
2022-01-01 23:23:41 +0000

    pause menu patterned BOIIIIIIIII

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -32,6 +32,7 @@ public class PauseMenu : MonoBehaviour
 
     public void loadMenu() {
         GameIsPaused = false;
+        Time.timeScale = 1f;
         SceneManager.LoadScene("StartMenu");
     }
 
