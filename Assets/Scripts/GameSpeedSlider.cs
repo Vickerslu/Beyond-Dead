6aa8be3ca722a6133c944db55448d2734b806b88
js340 <jaskaransingh3433@gmail.com>
2022-01-15 00:44:49 +0000

    Numbers to sliders in options menu, zombie sounds when player gets hit

--- Assets/Scripts/GameSpeedSlider.cs
+++ Assets/Scripts/GameSpeedSlider.cs
@@ -6,9 +6,15 @@ using UnityEngine.UI;
 public class GameSpeedSlider : MonoBehaviour
 {
     [SerializeField] private Slider _slider;
+    [SerializeField] private Text valueText;
     void Start()
     {
         Enemy.speedMultiplier = _slider.value;
         _slider.onValueChanged.AddListener(val => Enemy.speedMultiplier = val);
+        _slider.onValueChanged.AddListener(val => TextUpdate(val));
+    }
+    void TextUpdate(float value)
+    {
+        valueText.text = Mathf.RoundToInt(_slider.value * 100f) / 100f + "";
     }
 }
